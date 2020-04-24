81b268888f5b42eeae5e1d79fab88a2215f1dd65
Gio93k <gio93.cuccu@gmail.com>
2020-04-24 20:09:32 +0200

    FINITO

--- src/extensions/footerHeaderGioara/Components/MenuHeader.tsx
+++ src/extensions/footerHeaderGioara/Components/MenuHeader.tsx
@@ -5,105 +5,28 @@ import {
     IContextualMenuItem,
 } from 'office-ui-fabric-react/lib/ContextualMenu';
 import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
-import { sp } from '@pnp/sp';
 import { ListHelper } from '../../../Helpers/ListHelper';
-// import { ListHelper } from '../../../Helpers/ListHelper';
-// import { sp } from '@pnp/sp';
+import { Items } from '@pnp/sp/items';
 
-// export interface IMenuHeaderState{
-//     items: any[];
-// }
+export default class MenuHeader extends React.Component<{}, { menuItems: any }> {
 
-export default class MenuHeader extends React.Component<{}, {}> {
+    constructor(props) {
+        super(props)
+        this.state = { menuItems: [] };
+    }
 
     public render() {
         return (
             <div >
                 <div >
-                    <DefaultButton text="Click for ContextualMenu" persistMenu={true} menuProps={menuProps} />;
+                    <DefaultButton text="Click for ContextualMenu" persistMenu={true}
+                        menuProps={{ items: this.state.menuItems.map((val, i) => { return { key: i, text: val.Title, href: val.valore } }) }} />;
                 </div>
             </div>);
     }
 
     public async componentDidMount() {
         const items = await ListHelper.getList()
-        this.setState({ menuItems: items }); //ci vuole il map qui probabilmente
-
+        this.setState({ menuItems: items });
     }
 }
-
-
-const menuItems: IContextualMenuItem[] = [
-    {
-        key: 'newItem',
-        subMenuProps: {
-            items: [
-                {
-                    key: 'emailMessage',
-                    text: 'Email message',
-                    title: 'Create an email',
-                },
-                {
-                    key: 'calendarEvent',
-                    text: 'Calendar event',
-                    title: 'Create a calendar event',
-                },
-            ],
-        },
-        href: 'https://bing.com',
-        text: 'New',
-        target: '_blank',
-    },
-    {
-        key: 'divider_1',
-        itemType: ContextualMenuItemType.Divider,
-    },
-    {
-        key: 'rename',
-        text: 'Rename',
-        onClick: () => console.log('Rename clicked'),
-    },
-    {
-        key: 'edit',
-        text: 'Edit',
-        onClick: () => console.log('Edit clicked'),
-    },
-    {
-        key: 'properties',
-        text: 'Properties',
-        onClick: () => console.log('Properties clicked'),
-    },
-    {
-        key: 'linkNoTarget',
-        text: 'Link same window',
-        href: 'http://bing.com',
-    },
-    {
-        key: 'linkWithTarget',
-        text: 'Link new window',
-        href: 'http://bing.com',
-        target: '_blank',
-    },
-    {
-        key: 'linkWithOnClick',
-        name: 'Link click',
-        href: 'http://bing.com',
-        onClick: (ev: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement>) => {
-            alert('Link clicked');
-            ev.preventDefault();
-        },
-        target: '_blank',
-    },
-    {
-        key: 'disabled',
-        text: 'Disabled item',
-        disabled: true,
-        onClick: () => console.error('Disabled item should not be clickable.'),
-    },
-];
-
-const menuProps: IContextualMenuProps = {
-    shouldFocusOnMount: true,
-    shouldFocusOnContainer: true,
-    items: menuItems,
-};
